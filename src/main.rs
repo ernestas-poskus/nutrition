594a5cc1ab9752846e00ce70d9061a7d3c82503d
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-11-05 22:57:52 +0200

    Add base nutrition calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+
+const PROTEIN: f64 = 2.2; // 1.6 - 2.2
+
+const CARBOHYDRATES: f64 = 1.7; // 1.5 - 2.0
+
+const FAT: f64 = 0.65; // 0.4 - 0.8
+
 fn main() {
-    println!("Hello, world!");
+    let weight: Vec<f64> = env::args()
+        .skip(1)
+        .map(|a| a.parse::<f64>().unwrap_or(0.0))
+        .collect();
+
+    let weight = weight[0];
+
+    println!("Provided weight: {}kg", weight);
+    println!("protein: {:.0}g", weight * PROTEIN);
+    println!("carbohydrates: {:.0}g", weight * CARBOHYDRATES);
+    println!("fat: {:.0}g", weight * FAT);
 }
